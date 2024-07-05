2f865587bcac4d45bea6ae7990ee8475bdd8cf54
Rian Andri Waskito <rianandri1304@gmail.com>
2024-07-06 06:46:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,27 @@ const app = express();
 // Serve static files from the public directory
 app.use(express.static('public'));
 
+// API endpoint to handle date requests
 app.get('/api/:date?', (req, res) => {
   const dateString = req.params.date;
   let date;
 
+  // If no date is provided, use the current date
   if (!dateString) {
     date = new Date();
   } else if (!isNaN(dateString)) {
+    // If the date is a Unix timestamp (milliseconds since epoch)
     date = new Date(parseInt(dateString));
   } else {
+    // Otherwise, try to parse the date string
     date = new Date(dateString);
   }
 
+  // If the date is invalid, return an error
   if (date.toString() === 'Invalid Date') {
     res.json({ error: 'Invalid Date' });
   } else {
+    // Otherwise, return the Unix timestamp and UTC date string
     res.json({
       unix: date.getTime(),
       utc: date.toUTCString()
